d84bc7e68f0f38482f7996de0879ada73eeb955f
Eliza Wojtuściszyn <eliza.wojtusciszyn@gmail.com>
2026-03-22 12:00:32 +0100

    S01E02 - adding AccessLevel Client

---
+++ src/main/java/aidevs/course/s01e02/S01E02Runner.java
@@ -0,0 +1,31 @@
+package aidevs.course.s01e02;
+
+import aidevs.course.LessonRunner;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@ConditionalOnProperty(name = "spring.hub.lesson", havingValue = "s01e02")
+public class S01E02Runner implements LessonRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(S01E02Runner.class);
+
+    private final ObjectMapper objectMapper;
+
+    public S01E02Runner(
+            ObjectMapper objectMapper
+    ) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void run() throws IOException {
+        log.info("=== S01E02: Find Him ===");
+    }
+
+}
---
+++ src/main/java/aidevs/course/s01e02/tools/AccessLevelTool.java
@@ -0,0 +1,28 @@
+package aidevs.course.s01e02.tools;
+
+import aidevs.course.s01e02.clients.AccessLevelRestClient;
+import lombok.AllArgsConstructor;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class AccessLevelTool {
+
+    private final AccessLevelRestClient client;
+
+    @Tool(description = "Returns access level for given data")
+    public String accessLevel(
+            @ToolParam(description = "Individual api key")
+            String apikey,
+            @ToolParam(description = "Person name")
+            String name,
+            @ToolParam(description = "Person surname")
+            String surname,
+            @ToolParam(description = "Person birth year")
+            String birthYear
+    ) {
+        return client.post(apikey, name, surname, birthYear);
+    }
+}

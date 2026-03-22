42f60e3cb03acf9b9c81f33db7d5dc18575aa978
Eliza Wojtuściszyn <eliza.wojtusciszyn@gmail.com>
2026-03-22 13:07:33 +0100

    S01E02 - claculate coordinates

--- src/main/java/aidevs/course/s01e02/ChatService.java
+++ src/main/java/aidevs/course/s01e02/ChatService.java
@@ -1,6 +1,7 @@
 package aidevs.course.s01e02;
 
 import aidevs.course.s01e02.tools.AccessLevelTool;
+import aidevs.course.s01e02.tools.LocationTool;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,15 +11,18 @@ public class ChatService {
 
     private final ChatClient chatClient;
     private final AccessLevelTool accessLevelTool;
+    private final LocationTool locationTool;
 
     @Autowired
     public ChatService(ChatClient.Builder builder,
-                       AccessLevelTool accessLevelTool
+                       AccessLevelTool accessLevelTool,
+                       LocationTool locationTool
     ) {
         this.chatClient = builder.build()
                 .mutate()
-                .defaultTools(accessLevelTool)
+                .defaultTools(accessLevelTool, locationTool)
                 .build();
         this.accessLevelTool = accessLevelTool;
+        this.locationTool = locationTool;
     }
 }
